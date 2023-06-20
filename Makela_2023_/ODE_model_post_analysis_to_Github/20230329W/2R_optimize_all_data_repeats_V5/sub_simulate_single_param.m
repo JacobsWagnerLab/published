5b66adfea31274a440e19acbd2a165bc98f94389
Christine Jacobs-Wagner lab <32458801+JacobsWagnerLab@users.noreply.github.com>
2023-06-20 16:31:46 -0700

    Add files via upload

---
+++ Makela_2023_/ODE_model_post_analysis_to_Github/20230329W/2R_optimize_all_data_repeats_V5/sub_simulate_single_param.m
@@ -0,0 +1,30 @@
+
+function [simudata] = sub_simulate_single_param(param_temp, dataset_flag)
+
+%============================================================
+% Perform simulation
+simu = sub_DNA_model_V4(param_temp);
+
+%============================================================
+% Compare input initial condition with steady-state sol
+
+rec = NaN(1,6);  % [1] phiP [2] phi_SS [3] tau(exp) [4] tau(simu)
+cell_tau = [40 100 175];
+
+rec(1) = simu.F.phiP;
+rec(2) = simu.F.phi;    
+rec(3) = 1/rec(1);
+rec(4) = 1/rec(2);
+rec(5) = cell_tau(dataset_flag);
+rec(6) = simu.F.tau;
+
+% -----------------------------------------------------------
+
+simudata = {};
+simudata.traj = simu;
+simudata.rec = rec;
+
+end
+
+%============================================================
+
